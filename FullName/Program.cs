c8b94ecffb1144cb2614a75b4b2a43bae84ed6a6
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-03-02 16:55:02 -0600

    Finished Homework

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -6,6 +6,12 @@ namespace FullName
     {
         public static void Main()
         {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+            string fullName = FullName(firstName, lastName);
+            Console.WriteLine(fullName);
             // TODO: Ask the user for their first name and then their last name.
 
             // TODO: Call a method called FullName that returns their full name.
@@ -14,7 +20,14 @@ namespace FullName
 
             Console.ReadLine();
         }
+        public static string FullName(string firstName, string lastName)
+        {
+            string fullName = firstName + " " + lastName;
+            return fullName;
 
+        }
+
+    
         // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
     }
 }
